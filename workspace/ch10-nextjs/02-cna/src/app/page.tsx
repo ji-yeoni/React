03b036593917cc8bb7514a76c36c6ecad83fee5d
ji-yeoni <wldua16354@naver.com>
2025-06-30 18:05:46 +0900

    250630

---
+++ workspace/ch10-nextjs/02-cna/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Home 페이지입니다.'
+}
+
+
+export default function RootPage() {
+  return (
+    <h1>Home</h1>
+  );
+}
--- workspace/ch12-app/todolist/05-router/src/App.tsx
+++ workspace/ch12-app/todolist/05-router/src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router";
-import router from "./routes";
+// import router from "./routes";
+import router from "./routes-lazy";
 import './App.css';
+import { Suspense } from "react";
 
 function App() {
   return (  
+    <Suspense fallback={ <p>Loading...</p> }>
       <RouterProvider router={router}/>
+    </Suspense>
   );  
 }
 
